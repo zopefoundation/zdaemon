f1732b24514730993fc4d5fbb98f18d63f522937
Guido van Rossum <guido@python.org>
2002-11-09 17:18:03 +0000

    Cut some dead wood: remove the questionable "zinit" script that was living here.

--- __init__.py
+++ __init__.py
@@ -12,46 +12,6 @@
 # FOR A PARTICULAR PURPOSE
 #
 ##############################################################################
-"""
-
-zinit, slightly smarter server manager and ZServer startup script.
-
-  zinit will:
-
-    - Fork a parent and a child
-
-    - restart the child if it dies
-
-    - write a pid file so you can kill (the parent)
-
-    - reports the childs pid to stdout so you can kill that too
-
-TODO
-
-  - Have the parent reap the children when it dies
-
-  - etc.
-
-"""
+"""zdaemon -- a package to manage a daemon application."""
 
 from Daemon import run
-
-# XXX Is the following a useful feature?
-
-def main():
-    import sys
-    argv=sys.argv[1:]
-    if argv and argv[0][:2]=='-p':
-        pidf=argv[0][2:]
-        del argv[0]
-    else:
-        pidf=''
-
-    if not argv:
-        print __doc__ % vars()
-        print
-        print 'Error: no script given'
-
-    run(argv, pidf)
-
-if __name__ == '__main__': main()
