31f09ace8b69dff19bc1764447bf9ed20fa397df
Jim Fulton <jim@zope.com>
2006-11-05 17:43:40 +0000

    More release info.

--- setup.py
+++ setup.py
@@ -1,21 +1,43 @@
+entry_points = """
+[console_scripts]
+zdctl = zdaemon.zdctl:main
+zdrun = zdaemon.zdrun:main
+include_package_data=True,
+"""
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
 try:
     from setuptools import setup
+    setuptools_options = dict(
+        zip_safe=False,
+        entry_points=entry_points,
+        )
 except ImportError:
     from distutils.core import setup
+    setuptools_options = {}
 
+name = "zdaemon"
 setup(
-    name="zdaemon",
-    version="1.3",
-    url="http://svn.zope.org/zdaemon",
+    name=name,
+    version="1.4",
+    url="http://www.python.org/pypi/zdaemon",
     license="ZPL 2.1",
-    description="daemon process control library and tools",
-    long_description=open("README.txt").read(),
+    description="Daemon process control library and tools",
     author="Zope Corporation and Contributors",
     author_email="zope3-dev@zope.org",
+    long_description=(
+        read('README.txt')
+        + '\n' +
+        read('CHANGES.txt')
+        + '\n' +
+        'Download\n'
+        '**********************\n'
+        ),
 
     packages=["zdaemon", "zdaemon.tests"],
     package_dir={"": "src"},
-    include_package_data=True,
-    install_requires=["ZConfig", "zope.testing"],
-    zip_safe=False,
+    install_requires=["ZConfig"],
+    tests_require=["zope.testing"],
     )
