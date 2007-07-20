56e456121ccb76201c677450b7dcbdfd4efe90c9
Fred Drake <fdrake@acm.org>
2007-07-20 03:40:56 +0000

    prepare for 2.0.0 release

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ except ImportError:
 name = "zdaemon"
 setup(
     name=name,
-    version="2.0a6",
+    version="2.0.0",
     url="http://www.python.org/pypi/zdaemon",
     license="ZPL 2.1",
     description=
