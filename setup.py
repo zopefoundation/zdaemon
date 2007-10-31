8e7fdd9e0b24ae99d6afe8405f1cf679ada833a9
Stephan Richter <stephan.richter@gmail.com>
2007-10-31 04:49:13 +0000

    version bump.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ except ImportError:
 name = "zdaemon"
 setup(
     name=name,
-    version="2.0.1",
+    version="2.1.0",
     url="http://www.python.org/pypi/zdaemon",
     license="ZPL 2.1",
     description=
