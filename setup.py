a00bd368aa2ead3015619f72696763478f23fbae
Michael Howitz <mh@gocept.com>
2009-04-20 07:44:13 +0000

    Back to development: 2.0.5dev

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ except ImportError:
 name = "zdaemon"
 setup(
     name=name,
-    version = '2.0.4',
+    version = '2.0.5dev',
     url="http://www.python.org/pypi/zdaemon",
     license="ZPL 2.1",
     description=
