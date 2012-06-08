7423668a45255390d63ffa308a4d9826542287be
Jim Fulton <jim@zope.com>
2012-06-08 13:59:49 +0000

    Version.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ except ImportError:
 name = "zdaemon"
 setup(
     name=name,
-    version = '2.0.5dev',
+    version = '3.0.0',
     url="http://www.python.org/pypi/zdaemon",
     license="ZPL 2.1",
     description=
