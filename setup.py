0f2e5dda9b36b5465ec333458f0a6741662806b0
Marius Gedminas <marius@gedmin.as>
2013-02-15 13:38:24 +0000

    Preparing release 4.0.0a1

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ except ImportError:
 
 setup(
     name="zdaemon",
-    version = "4.0.0dev",
+    version='4.0.0a1',
     url="http://www.python.org/pypi/zdaemon",
     license="ZPL 2.1",
     description=
