ef30f8e247a586fe422f59e714688a845c049cec
Tres Seaver <tseaver@palladion.com>
2014-12-26 09:11:45 -0500

    svb

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ except ImportError:
 
 setup(
     name="zdaemon",
-    version='4.0.1',
+    version='4.0.2.dev0',
     url="http://www.python.org/pypi/zdaemon",
     license="ZPL 2.1",
     description=
