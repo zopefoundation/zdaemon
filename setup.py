32ee769cf3c6fd1c76832cedcd8caec5ff8f1d27
Marius Gedminas <marius@gedmin.as>
2015-04-16 08:30:32 +0300

    Back to development: 4.1.1

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ except ImportError:
 
 setup(
     name="zdaemon",
-    version='4.1.0',
+    version='4.1.1.dev0',
     url="https://github.com/zopefoundation/zdaemon",
     license="ZPL 2.1",
     description=
